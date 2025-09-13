d64d2443637d20095e8016459b30c2703322f4be
Daniel Damazio <daniel.d4mazio@gmail.com>
2025-09-13 19:07:56 -0300

    Base para um projeto onde o intuito é salvar o nome e email de um usuário.

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template('index.html')
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        username = request.form["user_name"]
+        useremail = request.form["user_email"]
+        return f"Usuário {username} e {useremail} registrado com sucesso!"
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run()
